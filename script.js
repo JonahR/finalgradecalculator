ff45e662e645c6235dfaef2a95ffe0cab0aee237
Jonah Rueb <ruebjonah@gmail.com>
2017-01-31 14:03:39 -0600

    Basic website structure

---
+++ script.js
@@ -0,0 +1,6 @@
+function calcGrade(){
+    var currentGrade = document.getElementById('currentGrade').value;
+    var finalGrade = document.getElementById('finalGrade').value;
+    var finalWorth = document.getElementById('finalWorth').value;
+    alert(currentGrade + " " + finalGrade + " " + finalWorth);
+}
